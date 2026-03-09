a0231881bf37d155649c0c477816782c8d1ef2ba
Meghana0218 <meghanabanki09@gmail.com>
2026-03-09 17:45:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -128,3 +128,26 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+
+    if(target){
+      target.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
